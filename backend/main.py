2f3794eab0c83c984a5d746eeb42e1dd2a3f906c
Tanushree Nerella <tanushreenerella697@gmail.com>
2026-06-13 03:00:10 +0530

    fix CORS - allow all origins

--- backend/main.py
+++ backend/main.py
@@ -29,9 +29,9 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
